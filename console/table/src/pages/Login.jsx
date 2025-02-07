9e5d056d3431e69a68d24cf2e108d62754944d75
zeeshan <7up.charsi@gmail.com>
2025-02-07 20:17:07 +0500

    fix: navigate on success res

--- console/table/src/pages/Login.jsx
+++ console/table/src/pages/Login.jsx
@@ -1,63 +1,64 @@
-import { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-import "../../src/styles/global.css"
-function Login (){
-  const [user , setUser] = useState({registrationNo : "", dob : ""})
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../../src/styles/global.css';
+function Login() {
+  const [user, setUser] = useState({ registrationNo: '', dob: '' });
 
-      
+  const navigate = useNavigate();
 
+  async function handleLogin(e) {
+    e.preventDefault();
+    try {
+      const res = await fetch('http://localhost:5000/api/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user),
+      });
 
-  async function handleLogin (e){
-      e.preventDefault();
-       try {
-        const response = await fetch("http://localhost:5000/api/login",{
-           method : "POST",
-           headers : {"Content-Type" : "application/json"
-           },
-           body : JSON.stringify(user)
-        })
-        const data = await response.json();
-        console.log("Login Response", data);
-         if(response.ok){
-            alert("Login Successfull")
-            localStorage.setItem("users", JSON.stringify(data.user))
-            setUser({registrationNo : "", dob : ""})
-            window.location.href = "/dashboard"
-         }else {
-          alert(data.message)
-         }  
-       } catch (error) {
-        console.error("Login Error", error);
-        alert("Something went Wroung")
-       } 
-       
+      if (!res.ok) throw await res.json();
 
+      navigate('/dashboard');
+    } catch (error) {
+      console.error('Login Error', error);
+
+      alert('Something went Wroung');
     }
+  }
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
-
-    function handleChange (e){
-      const {name,value} = e.target;
-       setUser((prev)=> ({
-          ...prev, 
-        [name] : value        
-       }))
-    }
-    return(
-      <>
-        <div className="container">
-          <h3>Login Page</h3>
-            <form onSubmit={handleLogin}>
-             <input type="text" name="registrationNo" placeholder="Registration No" onChange={handleChange} value={user.registrationNo} required />
-             <input type="date" name="dob" placeholder="Date of birth"  onChange={handleChange} value={user.dob} required/>
-             <button type="submit">Login</button>
-            </form>
-        </div>
-        
-      </>
-     )
-  
-    
+  return (
+    <>
+      <div className='container'>
+        <h3>Login Page</h3>
+        <form onSubmit={handleLogin}>
+          <input
+            type='text'
+            name='registrationNo'
+            placeholder='Registration No'
+            onChange={handleChange}
+            value={user.registrationNo}
+            required
+          />
+          <input
+            type='date'
+            name='dob'
+            placeholder='Date of birth'
+            onChange={handleChange}
+            value={user.dob}
+            required
+          />
+          <button type='submit'>Login</button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default Login;
